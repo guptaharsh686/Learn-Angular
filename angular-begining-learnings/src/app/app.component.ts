6e76779a9c046cd9be52b0d7ca5d62de3f501cc4
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-07 01:25:22 +0530

    if else usage using angular

--- angular-begining-learnings/src/app/app.component.ts
+++ angular-begining-learnings/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-begining-learnings';
+  show = true;
+
+  change(){
+    this.show= ! this.show
+  }
 }

6a01da30e25cebc2642af1d341665f7e8e47662b
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-08 00:09:21 +0530

    Done

--- angular-begining-learnings/src/app/app.component.ts
+++ angular-begining-learnings/src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-begining-learnings';
+
+  users = [
+    {
+      name:'Anil',
+      phone:1111,
+      socialAcc:[
+        'FB',
+        'Insta',
+        'Gmail'
+      ] 
+    },
+    {
+      name:'Sam',
+      phone:2222,
+      socialAcc:[
+        'FB',
+        'Insta',
+        'YT'
+      ]
+    },
+    {
+      name:'Peter',
+      phone:3333,
+      socialAcc:[
+        'FB',
+        'LINKDIN',
+        'Gmail'
+      ]
+    },
+    {
+      name:'Bruce',
+      phone:4444,
+      socialAcc:[
+        'FB',
+        'Insta',
+        'Gmail'
+      ]
+    },
+  ]
 }

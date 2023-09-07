5b4d1a420dc356d64729422e173090460adfc3f2
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-08 01:45:15 +0530

    Done

--- angular-begining-learnings/src/app/app.component.ts
+++ angular-begining-learnings/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-begining-learnings';
+
+  display = false;
+
+  toggle(){
+    this.display = !this.display;
+  }
 }

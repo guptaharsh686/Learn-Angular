20c908fd577e5b4773bf27127e7f68bf51c35521
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-09 16:12:18 +0530

    implemented todo list

--- ToDo-List-Angular/src/app/app.component.ts
+++ ToDo-List-Angular/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo-List-Angular';
+
+  list : any[] = []
+
+  addTask(item:string){
+    if(item !== '')
+    {
+      this.list.push({
+        id:this.list.length,
+        name: item
+      });
+      console.warn(this.list);
+    }
+    
+
+  }
+
+
+  removeTask(id:number){
+    this.list = this.list.filter(item => item.id !== id)
+  }
 }

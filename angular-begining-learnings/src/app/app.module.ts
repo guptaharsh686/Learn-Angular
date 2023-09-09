05eea5d0abdf5b2df416cbf6fa3e1dc4cb71780d
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-09 23:19:03 +0530

    Done

--- angular-begining-learnings/src/app/app.module.ts
+++ angular-begining-learnings/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { USDtoINRPipe } from './pipt/usdto-inr.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    USDtoINRPipe
   ],
   imports: [
     BrowserModule

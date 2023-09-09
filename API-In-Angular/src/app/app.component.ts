3f720f8f34fd3edc1a5a5e6d7e7c41044b3aff01
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-10 02:41:06 +0530

    Done

---
+++ API-In-Angular/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {UsersdataService} from './Services/usersdata.service'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'API-In-Angular';
+  users:any
+  constructor(private userdata:UsersdataService){
+    userdata.users().subscribe((data) => {
+      this.users = data;
+      console.warn(data)
+    })
+
+  }
+}

525d953cea2dec2985e0f2d7e41546e659dea833
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-10 00:42:43 +0530

    Done

---
+++ Learn-Angular-Routing/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+  path: 'about',
+  component : AboutComponent
+  },
+  {
+    path: 'user',
+    component : UserComponent
+  },
+  {
+    path: '',
+    component : HomeComponent
+  }
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

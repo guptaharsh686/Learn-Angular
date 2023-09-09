32d4023757cd05a871036f37ec96335b67b9356b
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-10 00:10:00 +0530

    Done

--- angular-begining-learnings/src/app/app.component.ts
+++ angular-begining-learnings/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl,FormGroup,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-begining-learnings';
+
+  loginform = new FormGroup(
+    {user : new FormControl('',[Validators.required,Validators.email]),
+    password : new FormControl('',[Validators.required,Validators.minLength(5)])}
+  )
+
+  loginuser(){
+    console.warn(this.loginform.value)
+  }
+
+  get user(){
+    return this.loginform.get('user')
+  }
+
+  get password(){
+    return this.loginform.get('password')
+  }
 }

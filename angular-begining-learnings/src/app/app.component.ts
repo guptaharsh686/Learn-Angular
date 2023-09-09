1bfe50299d02b54b9dc1f4a780cdcb06ede85342
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-09 19:43:19 +0530

    Done

--- angular-begining-learnings/src/app/app.component.ts
+++ angular-begining-learnings/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-begining-learnings';
+
+
+  getValue(item : HTMLInputElement){
+    console.warn(item);
+  }
 }
